b84b9da7dc5726c4fa3ca343cdd4f2a6add155d9
saetlan <s.arnaud67@hotmail.fr>
2015-05-30 06:09:45 +0200

    CSS visu scale

--- resources/views/escale/show.blade.php
+++ resources/views/escale/show.blade.php
@@ -5,15 +5,39 @@
 		<div class="alert alert-error">{!! session('error') !!}</div>
 	@endif
 	
-	<h2>Mouvements effectués pour l'escale {{$id}}</h2>
-	
-	<h3>Chargements effectués (NB = {{$nb_chargements}})</h3>
-	@foreach ($chargements as $c)
-		<p> {{$c->date_mouvement}} : chargement du conteneur {{$c->id_conteneur}} </p>
-	@endforeach
-	
-	<h3>Déchargements effectués (NB = {{$nb_dechargements}})</h3>
-	@foreach ($dechargements as $d)
-		<p> {{$c->date_mouvement}} : chargement du conteneur {{$c->id_conteneur}} </p>
-	@endforeach
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="panel panel-default">
+					<div class="panel-heading">Mouvements effectués pour l'escale {{$id}}</div>
+						<div class="panel-body">
+							<div class="form-horizontal">
+
+								<div class="form-group">
+									<div class="col-md-6">
+										<label style="col-md-4 control-label">Chargements effectués (NB = {{$nb_chargements}})</label>
+										@foreach ($chargements as $c)
+											<p> {{$c->date_mouvement}} : chargement du conteneur {{$c->id_conteneur}} </p>
+										@endforeach
+									</div>	
+
+									
+								</div>
+								<div class="form-group">
+									<div class="col-md-6">
+										<label style="col-md-4 control-label">Déchargements effectués (NB = {{$nb_dechargements}})</label>
+										@foreach ($dechargements as $d)
+											<p> {{$c->date_mouvement}} : déchargement du conteneur {{$c->id_conteneur}} </p>
+										@endforeach
+									</div>	
+								</div>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 @endsection
