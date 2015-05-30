dca9208bd42dc512b4816617512be38e4cac4979
Yannis Weishaupt <yannis.weishaupt@outlook.com>
2015-05-30 04:28:18 +0200

    filliable conteneur

--- app/Conteneur.php
+++ app/Conteneur.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Conteneur extends Model {
 
 	protected $table = 'conteneur';
+	protected $fillable = array('taille', 'id_user');
 	public $timestamps = true;
 
 }
