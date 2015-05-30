77e25cd8202c744863410e396da559b8cff221fa
saetlan <s.arnaud67@hotmail.fr>
2015-05-30 05:05:24 +0200

    CSS mouvement

--- resources/views/mouvement/create.blade.php
+++ resources/views/mouvement/create.blade.php
@@ -4,37 +4,54 @@
 	@if(session()->has('message'))
 		<div class="alert alert-success">{!! session('message') !!}</div>
 	@endif
-	{!! Form::open(array('route' => 'mouvement.store', 'method' => 'POST')) !!}
-		<ul>
-			<li>
-				{!! Form::label('date_mouvement', 'Date du mouvement : ') !!}
-				{!! Form::text('date_mouvement') !!}
-			</li>
-			<li>
-				{!! Form::label('id_escale', 'Id de l\'escale : ') !!}
-				<select name="id_escale" id="id_escale">
-				@foreach ($escales as $e)
-					<option value="{{$e->id}}">{{$e->id}}</option>
-				@endforeach
-				</select>
-			</li>
-			<li>
-				{!! Form::label('id_conteneur', 'Id du conteneur : ') !!}
-				<select name="id_conteneur" id="id_conteneur">
-				@foreach ($conteneurs as $c)
-					<option value="{{$c->id}}">{{$c->id}}</option>
-				@endforeach
-				</select>
-			</li>
-			<li>
-				<input type="radio" name="chargement" id="true">
-				{!! Form::label('true', 'Chargement') !!}
-				<input type="radio" name="chargement" id="false">
-				{!! Form::label('false', 'Dechargement') !!}
-			</li>
-			<li>
-				{!! Form::submit() !!}
-			</li>
-		</ul>
-	{!! Form::close() !!}
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="panel panel-default">
+					<div class="panel-heading">Création d'une escale</div>
+						<div class="panel-body">
+							<div class="form-horizontal">
+								{!! Form::open(array('route' => 'mouvement.store', 'method' => 'POST')) !!}
+							
+								<div class="form-group">
+									{!! Form::label('date_mouvement', 'Date du mouvement : ', ['class'=>'col-md-4 control-label']) !!}
+									<div class="col-md-6">
+										{!! Form::text('date_mouvement','',['class'=>'form-control']) !!}
+									</div>
+								</div>
+
+								<div class="form-group">
+									{!! Form::label('id_conteneur', 'Id du conteneur : ',['class'=>'col-md-4 control-label']) !!}
+									<div class="col-md-6">
+										<select name="id_conteneur" id="id_conteneur" class="form-control">
+											@foreach ($conteneurs as $c)
+												<option value="{{$c->id}}">{{$c->id}}</option>
+											@endforeach
+										</select>
+									</div>	
+								</div>
+
+								<div class="form-group">
+									{!! Form::label('true', 'Choix du mouvement',['class'=>'col-md-4 control-label']) !!}
+									<div class="col-md-6">
+										<select name="Chargement" id="Chargement" class="form-control">
+											<option value="true">Chargement</option>
+											<option value="false">Déchargement</option>
+										</select>
+									</div>
+								</div>	
+
+								<div class="form-group">
+									<div class="col-md-6 col-md-offset-4">
+										{!! Form::submit('Valider', ['class'=>'btn btn-primary', 'id'=>'tamere']) !!}
+									</div>
+								</div>
+								{!! Form::close() !!}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
