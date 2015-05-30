07d35c354a91970cc1b8cf079e1b2e30d753be54
Jean-Marcellin Truong <mini.fusee@hotmail.Fr>
2015-05-30 05:01:17 +0200

    escale css

--- resources/views/escale/index.blade.php
+++ resources/views/escale/index.blade.php
@@ -1,15 +1,39 @@
 @extends('app')
- 
+
 @section('content')
 	@if(session()->has('message'))
-		<div class="alert alert-error">{!! session('error') !!}</div>
+		<div class="alert alert-success">{!! session('message') !!}</div>
 	@endif
-	{!! Form::open(array('route' => 'escale.show', 'method' => 'GET')) !!}
-		<select name="id" id="id">
-		@foreach ($escales as $e)
-			<option value="{{$e->id}}">Escale du navire d'id {{$e->id_navire}} du {{$e->date_entree}} au {{$e->date_sortie}}</option>
-		@endforeach
-		</select>
-		{!! Form::submit() !!}
-	{!! Form::close() !!}
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="panel panel-default">
+					<div class="panel-heading">Choix d'une escale</div>
+						<div class="panel-body">
+							<div class="form-horizontal">
+                {!! Form::open(array('route' => 'escale.show', 'method' => 'GET')) !!}
+								<div class="form-group">
+									{!! Form::label('escale', 'Escale : ', ['class'=>'col-md-4 control-label']) !!}
+									<div class="col-md-6">
+                    <select name="id" id="id" class="form-control">
+                    @foreach ($escales as $e)
+                      <option value="{{$e->id}}">Escale du navire d'id {{$e->id_navire}} du {{$e->date_entree}} au {{$e->date_sortie}}</option>
+                    @endforeach
+                    </select>
+									</div>
+								</div>
+
+									<div class="col-md-6 col-md-offset-4">
+										{!! Form::submit('Envoyer',['class'=>'btn btn-primary', 'id'=>'tamere']) !!}
+									</div>
+								</div>
+								{!! Form::close() !!}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 @endsection
