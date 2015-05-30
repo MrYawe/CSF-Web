cf0464ffcc5ba32770ff4d0139a8b64988191de0
saetlan <s.arnaud67@hotmail.fr>
2015-05-30 02:53:50 +0200

    ccs changes

--- resources/views/navire/create.blade.php
+++ resources/views/navire/create.blade.php
@@ -1,9 +1,21 @@
 @extends('app')
  
 @section('content')
-    <h2>Create Navire</h2>
- 
-    {!! Form::model(new App\Navire, ['route' => ['navire.store']]) !!}
-        @include('navire/_form', ['submit_text' => 'Create Project'])
-    {!! Form::close() !!}
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				
+				<div class="panel-heading">Création d'un Navire</div>
+	 				<div class="panel-body">
+	 					<div class="form-horizontal">
+	 						{!! Form::model(new App\Navire, ['route' => ['navire.store']]) !!}
+					        @include('navire/_form', ['submit_text' => 'Create Project'])
+					    	{!! Form::close() !!}
+	 					</div>
+					</div>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
